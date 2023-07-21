b89e2624ec6e044404ab9e27d81b203e2d8c6a97
JebaSolomonRajS <mynewworkmail007@gmail.com>
2023-07-21 12:19:44 +0530

    vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    loader: "custom",
-    loaderFile: "./components/image.js",
-  },
+  // images: {
+  //   loader: "custom",
+  //   loaderFile: "./components/image.js",
+  // },
   reactStrictMode: true,
   swcMinify: true,
-  distDir: "build",
-  output: "export",
+  // distDir: "build",
+  // output: "export",
 };
 
 module.exports = nextConfig;
